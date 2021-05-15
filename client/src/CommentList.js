94a91e59656dc67f2a3cf4cfefdb48425d5b0c30
Vinod Gupta <VinodG@MITR-2MG1HW.local>
2021-05-15 20:50:14 +0530

    MS app

---
+++ client/src/CommentList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    } else if (comment.status === "rejected") {
+      content = "This comment has been rejected!";
+    } else if (comment.status === "pending") {
+      content = "This comment is awaiting moderation!";
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
